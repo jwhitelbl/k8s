bc42b83c05005079219c261adea8823d99e61eab
John White <jwhite@lbl.gov>
2022-10-26 21:31:23 -0700

    simplify unpack

--- transmission/unpack.sh
+++ transmission/unpack.sh
@@ -3,23 +3,12 @@ echo "============" >> /config/unpack.log
 date >> /config/unpack.log
 echo "$TR_TORRENT_DIR/$TR_TORRENT_NAME" >> /config/unpack.log
 cd $TR_TORRENT_DIR/$TR_TORRENT_NAME
-FIRST=`ls *.rar`
-SECOND=`ls *.r00`
-THIRD=`ls *.r01`
+FILE=`ls | grep -E 'rar|r00' | tail -1`
 
-if [ "$FIRST" != "" ]; then
-	echo "$FIRST" >> /config/unpack.log
-        /usr/local/bin/unrar x "$FIRST" | tail -5 >> /config/unpack.log
+if [ "$FILE" != "" ]; then
+	echo "$FILE" >> /config/unpack.log
+        /usr/local/bin/unrar x "$FILE" | tail -5 >> /config/unpack.log
 	exit 0
-fi
-if [ "$SECOND" != "" ]; then
-	echo "$SECOND" >> /config/unpack.log
-        /usr/local/bin/unrar x "$SECOND" | tail -5 >> /config/unpack.log
-	exit 0
-fi
-if [ "$THIRD" != "" ]; then
-	echo "$THIRD" >> /config/unpack.log
-	/usr/local/bin/unrar x "$THIRD" | tail -5 >> /config/unpack.log
 else
 	echo "no unpack needed" >> /config/unpack.log
 fi
